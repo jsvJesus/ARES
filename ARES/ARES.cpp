8f8702eb64ec9e909c2c922d19bb65123ccc1c19
Jesus <52017003+jsvJesus@users.noreply.github.com>
2026-02-20 19:27:59 +0200

    Delete ARES directory

--- ARES/ARES.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ARES.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ARES, "ARES" );
-
-DEFINE_LOG_CATEGORY(LogARES)
- 
